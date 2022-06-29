7569784e7afaf5e4c6e1f842b556b8e3acd8cedc
sop51 <smarcotte11@gmail.com>
2022-06-29 15:40:32 +0200

    Initial Commit

---
+++ iDine/iDineApp.swift
@@ -0,0 +1,17 @@
+//
+//  iDineApp.swift
+//  iDine
+//
+//  Created by Sophie Marcotte on 6/29/22.
+//
+
+import SwiftUI
+
+@main
+struct iDineApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
